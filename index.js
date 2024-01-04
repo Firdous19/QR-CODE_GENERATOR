b4484f4b3410c388fd3f346f6179acb83c0a24e0
Firdous <firdousrahaman42@gmail.com>
2024-01-04 10:27:02 +0530

    Added code for generating qr image

---
+++ index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+    .prompt([
+        { message: "Type in your URL", name: "url" }
+    ])
+    .then((answers) => {
+        const url = answers.url;
+        const qr_svg = qr.image(url, { type: 'png' });
+        qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+
+        }
+    });
